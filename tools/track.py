2792d3aea2724799a7972b0474f3fdf79a3cb004
Giulio Santini <giulio.santini@gmail.com>
2021-05-10 00:26:27 -0400

    added most of the trackrc logic

--- tools/track.py
+++ tools/track.py
@@ -31,14 +31,37 @@ parser.add_argument('-l', '--list', help='display last 5 lines of todays file', 
 parser.add_argument('-m', '--message_current', help='append newline, date, message')
 parser.add_argument('-a', '--message_additional', help='append newline, message')
 parser.add_argument('-p', '--message_previous', help='append message, newline, date')
-parser.add_argument('-d', '--log_dir', help='set and save log_dir', action='store_true')
-parser.parse_args()
+parser.add_argument('-d', '--log_dir', help='set and save log_dir')
+args = parser.parse_args()
 
 # mkdir -p ~/.config/track
 confdir = Path(Path.home(), '.config', 'track') 
 confdir.mkdir(parents=True, exist_ok=True)
 
+trackrc = Path(confdir, 'trackrc')
+trackrc.touch(exist_ok=True)
 
+brkpoint = '0400'
+
+logdir = Path.cwd()
+if args.log_dir:
+    logdir = Path(args.log_dir)
+    if not logdir.is_dir():
+        print('please specify a valid log directory')
+        exit(1)
+
+# pull info from trackrc, update if necessary
+# TODO deal with first run case, no args, no trackrc
+with open(str(trackrc), 'r+') as f:
+    content = f.read()
+    if len(content) > 0:
+        logdir_stored, brkpoint_stored = [x.split(':')[1] for x in content.split('\n')]
+    f.seek(0)
+    #TODO do something with setting breakpoint?
+    brkpoint = brkpoint_stored
+    if not args.log_dir:
+        logdir = logdir_stored
+    f.write('logdir:{}\nbreakpoint:{}'.format(logdir, brkpoint))
 
 print('hello')
 
